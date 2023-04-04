11c3011b69515737eedecc2bdbc14a94ef2eda8e
Mariusz Politowicz <politowiczmar@gmail.com>
2023-04-04 22:15:19 +0200

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Container } from "react-bootstrap";
+import { Footer } from "./components/views/Footer";
 import { Navigation } from "./components/views/Navigation";
 
 export const App = () => {
   return (
     <main>
-      <Container>
+      <Container
+        style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+      >
         <Navigation />
         <h1>Hello World</h1>
+        <Footer />
       </Container>
     </main>
   );
