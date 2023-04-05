e59f20e48e637cc9eda3c415add895448493fdb7
Mariusz Politowicz <politowiczmar@gmail.com>
2023-04-05 22:50:22 +0200

    Delete unused data from Home Component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,10 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchTables } from "../../redux/tablesRedux";
-import { Table } from "../common/Table";
+import { RenderTable } from "../features/RenderTable";
 
 export const Home = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => fetchTables(dispatch), [dispatch]);
-
   return (
     <>
       <h1>All tables</h1>
-      <Table />
+      <RenderTable />
     </>
   );
 };
