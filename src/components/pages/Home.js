3cd16a9005b66e3b90956cebedfa07f57bf3492d
Mariusz Politowicz <politowiczmar@gmail.com>
2023-04-05 22:22:05 +0200

    Fetch tables data from json-server

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchTables } from "../../redux/tablesRedux";
+import { Table } from "../common/Table";
+
 export const Home = () => {
-  return <h1>All tables</h1>;
+  const dispatch = useDispatch();
+
+  useEffect(() => fetchTables(dispatch), [dispatch]);
+
+  return (
+    <>
+      <h1>All tables</h1>
+      <Table />
+    </>
+  );
 };
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,11 +1,21 @@
 /* SELECTORS */
+
 /* ACTIONS */
 const createActionName = (actionName) => `app/tables/${actionName}`;
+const UPDATE_TABLES = createActionName("UPDATE_TABLES");
 
 /* ACTION CREATORS */
+export const updateTables = (payload) => ({ type: UPDATE_TABLES, payload });
+export const fetchTables = (dispatch) => {
+  fetch("http://localhost:3131/api/tables")
+    .then((res) => res.json())
+    .then((data) => dispatch(updateTables(data)));
+};
 
 export const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
+    case UPDATE_TABLES:
+      return [...action.payload];
     default:
       return statePart;
   }
