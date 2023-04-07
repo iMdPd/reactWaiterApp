a76da9a39ce909b77ca0ecd8cb6332826f298a20
Mariusz Politowicz <politowiczmar@gmail.com>
2023-04-07 15:51:20 +0200

    Use map on tables in Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,19 @@
 import { RenderTable } from "../features/RenderTable";
+import { selectTables } from "../../redux/tablesRedux";
+import { Loading } from "../features/Loading";
+import { useSelector } from "react-redux";
 
 export const Home = () => {
+  const tablesData = useSelector(selectTables);
+
   return (
     <>
       <h1>All tables</h1>
-      <RenderTable />
+      {tablesData.length === 0 ? (
+        <Loading />
+      ) : (
+        tablesData.map((table) => <RenderTable key={table.id} {...table} />)
+      )}
     </>
   );
 };
